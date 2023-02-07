106cbff8add2b0d5313101d774ccb964e0469c24
yogeii <yogeshkumarmotwani4@gmail.com>
2023-02-07 23:50:51 +0500

    updated script

--- Mac_changer.py
+++ Mac_changer.py
@@ -1,35 +1,61 @@
-#!/bin/python3
-
-import subprocess
 import argparse
-# import pyfiglet as pfg
+import subprocess
+import re
 
-# UI code
-def UI():
-    # banner = pfg.figlet_format("Mac Changer",font="slant")
-    # print(banner)
+def input_validator(user_specified_mac,user_specified_interface):
+    if user_specified_mac and user_specified_interface:
+        mac_change(user_specified_mac,user_specified_interface)
+    elif not user_specified_mac:
+        print("The user did not specify mac address")
+    elif not user_specified_interface:
+        print("The user did not specify interface")
 
-    parser = argparse.ArgumentParser(description="Python MAC Changer")
-    parser.add_argument('-nm',metavar='New Macaddress',help="Specify the new mac or the spoofed mac")
-    parser.add_argument('-int',metavar='Specify Interface ',help="Specify the interface to be changed")
-    args = parser.parse_args()
-    global nm
-    nm = args.nm
-    global intr 
-    intr = args.int
-    if not nm:
-        print("no mac was specified")
-    elif not intr:
-        print("no interface was specified")
     
-    mac_change(nm,intr)
+def check_current_mac(interface):
+    csm = str(subprocess.check_output(['ifconfig',interface]))
+    current_system_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",csm)
+    return current_system_mac.group(0)
+
+
+def check_new_mac(interface):
+    csm = str(subprocess.check_output(['ifconfig',interface]))
+    new_system_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",csm)
+    return new_system_mac.group(0)
 
 def mac_change(newmac,interface):
-    mac_to_spoof = newmac
+    mac_addr_spoof = newmac
     network_interface = interface
+    
+    old_mac = check_current_mac(user_specified_interface)
 
+    # System commands 
     subprocess.call(['ifconfig',network_interface,"down"])
-    subprocess.call(['ifconfig',network_interface,"hw","ether",mac_to_spoof])
+    subprocess.call(['ifconfig',network_interface,"hw","ether",mac_addr_spoof])
     subprocess.call(['ifconfig',network_interface,"up"])
-# function call
+
+    new_mac = check_new_mac(user_specified_interface)
+
+    print(f'The mac address has been changed from {old_mac} to {new_mac}')
+
+def UI():
+    # Display
+    banner = "****** Python Mac Changer ******"
+    print(banner)
+
+    # user options
+    parser = argparse.ArgumentParser(description="Python MAC Changer")
+    parser.add_argument('-n',metavar=' New Mac address ',help="Specify the new mac or the mac address you want to spoof")
+    parser.add_argument('-i',metavar=' Specify Interface ',help="Specify the interface to be changed")
+    args = parser.parse_args()
+    
+    # getting user input
+    global user_specified_mac
+    global user_specified_interface
+    user_specified_mac =  args.n
+    user_specified_interface = args.i
+
+    #Checking for valid inputs  
+    input_validator(user_specified_mac,user_specified_interface)
+
+# program start
 UI()
